0609461b57e7d9268f54a5227066e25f2c1508b2
Badal Aryal <badal.aryal@gmail.com>
2023-07-31 16:42:01 +0545

    all the assets maintained in app successfully

--- Facebook- with animation and transition/ViewController.swift
+++ Facebook- with animation and transition/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let bgImageView : UIImageView = {
+        let  imageView = UIImageView(named: "fbNews")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(bgImageView)
+        bgImageView.frame = view.frame
+        setupLongPressGesture()
+    
+    filePrivate func setupLongPressGesture() {
+        
+        
     }
-
-
+    override var prefersStatusBarHidden: Bool{
+       
+        
+        return true
+    }
+    
 }
 
